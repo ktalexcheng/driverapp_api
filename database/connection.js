743c031f561d6c21ecff16a6440b83fb1b4cc49f
Alex Cheng <27754503+ktalexcheng@users.noreply.github.com>
2022-09-25 08:12:09 +0800

    Use SSL authentication with database

--- database/connection.js
+++ database/connection.js
@@ -4,11 +4,11 @@ const mongoose = require('mongoose');
 // Configure connection to database
 mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
-    // ssl: true,
-    // sslValidate: true,
-    // sslCert: `${__dirname}/../certificate/X509-cert.pem`,
-    // sslKey: `${__dirname}/../certificate/X509-cert.pem`,
-    // authMechanism: 'MONGODB-X509',
+    ssl: true,
+    sslValidate: true,
+    sslCert: `${__dirname}/../certificate/atlas-admin-X509-cert.pem`,
+    sslKey: `${__dirname}/../certificate/atlas-admin-X509-cert.pem`,
+    authMechanism: 'MONGODB-X509',
 });
 
 // Connect to database
