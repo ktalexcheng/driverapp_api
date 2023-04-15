b254a17575adb08abea11dfa5a6ed1e36165ee93
Alex Cheng <27754503+ktalexcheng@users.noreply.github.com>
2023-04-15 21:31:41 +0800

    Fix bug in HTTPWriteJSONBody()

--- util/response.go
+++ util/response.go
@@ -12,15 +12,18 @@ type JSONResponse struct {
 
 // Utility to write status code to response header and JSONResponse to body
 func HTTPWriteJSONBody(w http.ResponseWriter, status int, respBody interface{}) error {
-	response, err := json.Marshal(respBody)
-	if err != nil {
-		return err
-	}
-
 	w.WriteHeader(status)
-	_, err = w.Write(response)
-	if err != nil {
-		return err
+
+	if respBody != nil {
+		response, err := json.Marshal(respBody)
+		if err != nil {
+			return err
+		}
+
+		_, err = w.Write(response)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
